0ad031f642a6ee0854c33c55656cfe6bb828e872
ashkan-R <45257713+ashkan-R@users.noreply.github.com>
2021-02-22 09:47:07 +0100

    Add files via upload

---
+++ Hardware.java
@@ -0,0 +1,63 @@
+public class Hardware {
+
+    private int id;
+    private String name;
+    private String ipAddress;
+    private String passwort;
+    private String beschreibung;
+
+    public Hardware(int id, String name, String ipAddress, String passwort, String beschreibung) {
+        this.id = id;
+        this.name = name;
+        this.ipAddress = ipAddress;
+        this.passwort = passwort;
+        this.beschreibung = beschreibung;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public static Hardware hardwareConfigurieren(int id, String name, String ipAddress, String passwort, String beschreibung) {
+        return new Hardware(id, name, ipAddress, passwort, beschreibung);
+    }
+
+}
---
+++ Kunde.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class Kunde extends Person {
+
+    ArrayList<Hardware> hardwareliste = new ArrayList<Hardware>();
+    ArrayList<Software> softwareliste = new ArrayList<Software>();
+
+
+    public Kunde(int id, String vorname, String nachname, String address, String wohnort, String postleitzahl, String telefonnummer) {
+        super(id, vorname, nachname, address, wohnort, postleitzahl, telefonnummer);
+    }
+
+    public boolean hardwareaddieren(Hardware hardware) {
+
+        if (hardwareliste.contains(hardware)) {
+
+            System.out.println("Hardware ist bereits in die Liste");
+            return false;
+        }
+        hardwareliste.add(hardware);
+        return true;
+    }
+
+    public boolean softwareaddieren(Software software) {
+
+        if (softwareliste.contains(software)) {
+            System.out.println("Software ist bereits in die Liste");
+            return false;
+        }
+        softwareliste.add(software);
+        return true;
+
+    }
+
+    public void softwareaktualisiren(Software zugangdaten, Software nuezugangdaten) {
+
+        this.softwareliste.set(zugangdaten.getId()  , nuezugangdaten);
+        System.out.println("Zugansdaten aktualisiert");
+
+
+    }
+
+
+    public boolean hardwareEntfernen(Hardware hardware) {
+        if (hardwareliste.contains(hardware)){
+            System.out.println("Diese Objekt wurde nicht gefunden");
+            return false;
+
+        }
+        this.hardwareliste.remove(hardware);
+        return true;
+
+    }
+
+
+
+
+
+
+
+
+}
