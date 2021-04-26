2e75d43eb77ea336959b050385ff7c0f61d82e5e
Slinus02 <77145268+Slinus02@users.noreply.github.com>
2021-04-26 12:33:26 +0200

    Add files via upload

---
+++ MySQLTest/src/MySQLTest/Main.java
@@ -0,0 +1,13 @@
+package MySQLTest;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		 MySQLAccess dao = new MySQLAccess();
+		 dao.readDataBase();
+		//MySQLWrite dao = new MySQLWrite();
+		//dao.mySQLWrite(args);
+		
+		
+	}
+
+}
