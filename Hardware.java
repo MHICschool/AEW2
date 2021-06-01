1a8b0391b6fa7ec7e9aafcd0c6db85da1f3a13da
Scarface1992 <78533771+Scarface1992@users.noreply.github.com>
2021-06-01 13:26:55 +0200

    Add files via upload

--- Hardware.java
+++ Hardware.java
@@ -1,63 +1,66 @@
+package Projekt;
+
 public class Hardware {
 
-    private int id;
-    private String name;
-    private String ipAddress;
-    private String passwort;
-    private String beschreibung;
+private int id;
+private String name;
+private String ipAddress;
+private String passwort;
+private String beschreibung;
 
-    public Hardware(int id, String name, String ipAddress, String passwort, String beschreibung) {
-        this.id = id;
-        this.name = name;
-        this.ipAddress = ipAddress;
-        this.passwort = passwort;
-        this.beschreibung = beschreibung;
-    }
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+public Hardware(int id, String name, String ipAddress, String passwort, String beschreibung) {
+    this.id = id;
+    this.name = name;
+    this.ipAddress = ipAddress;
+    this.passwort = passwort;
+    this.beschreibung = beschreibung;
+}
 
 
-    public String getIpAddress() {
-        return ipAddress;
-    }
+public int getId() {
+    return id;
+}
 
-    public void setIpAddress(String ipAddress) {
-        this.ipAddress = ipAddress;
-    }
+public void setId(int id) {
+    this.id = id;
+}
 
-    public String getPasswort() {
-        return passwort;
-    }
+public String getName() {
+    return name;
+}
 
-    public void setPasswort(String passwort) {
-        this.passwort = passwort;
-    }
+public void setName(String name) {
+    this.name = name;
+}
 
-    public String getBeschreibung() {
-        return beschreibung;
-    }
 
-    public void setBeschreibung(String beschreibung) {
-        this.beschreibung = beschreibung;
-    }
+public String getIpAddress() {
+    return ipAddress;
+}
 
-    public static Hardware hardwareConfigurieren(int id, String name, String ipAddress, String passwort, String beschreibung) {
-        return new Hardware(id, name, ipAddress, passwort, beschreibung);
-    }
+public void setIpAddress(String ipAddress) {
+    this.ipAddress = ipAddress;
+}
+
+public String getPasswort() {
+    return passwort;
+}
+
+public void setPasswort(String passwort) {
+    this.passwort = passwort;
+}
+
+public String getBeschreibung() {
+    return beschreibung;
+}
+
+public void setBeschreibung(String beschreibung) {
+    this.beschreibung = beschreibung;
+}
+
+public static Hardware hardwareConfigurieren(int id, String name, String ipAddress, String passwort, String beschreibung) {
+    return new Hardware(id, name, ipAddress, passwort, beschreibung);
+}
 
 }
+
